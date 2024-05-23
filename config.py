7a6725d4386398aa44fc68413b7eeaa75fcd733b
aditya94139 <152467027+aditya94139@users.noreply.github.com>
2024-05-23 11:03:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-api_id = 23163380
-api_hash = "2dca155e786c7db2d295e5b4ab10783b"
-bot_token = "6765960779:AAGrk1Mf2Tn-KMdsL2B9E4DdgZuBh1Rm42I"
+api_id = 24537816
+api_hash = "d88c4e65689faa570fbc5949468b5c61"
+bot_token = "6604752400:AAHR3kfNW4cSYIpijBifWI-vQkAPo-kHeqc"
 auth_users = "1368753935,5827915041"
 sudo_users = [int(num) for num in auth_users.split(",")]
 osowner_users = "1368753935,5827915041"
